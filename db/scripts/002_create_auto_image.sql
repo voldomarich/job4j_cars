2bddc064b059a7ed927b740dd65b925600884bfd
voldomarich <vbekasov1987@gmail.com>
2025-07-09 00:14:09 +0300

    2. Интеграционные тесты для проекта job4j_cars [#4746 #543008]

--- db/scripts/002_create_auto_image.sql
+++ db/scripts/002_create_auto_image.sql
@@ -1,7 +1,5 @@
 CREATE TABLE IF NOT EXISTS auto_image (
     id    SERIAL PRIMARY KEY,
     name  VARCHAR(255) NOT NULL,
-    path  VARCHAR(255) NOT NULL,
-    post_id int references auto_post(id)
+    path  VARCHAR(255) NOT NULL
 );
-
